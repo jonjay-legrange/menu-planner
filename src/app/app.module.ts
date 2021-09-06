661752ef557849b61f77585fc93bfda2c08b22ae
Jon Jay Le Grange <jonjay.legrange@blv.admin.ch>
2021-09-06 20:07:24 +0200

    Course Project - Components & Databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingComponent } from './header/shopping/shopping.component';
-import { ShoppingListComponent } from './header/shopping/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './header/shopping/shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipesComponent } from './header/recipes/recipes.component';
-import { RecipeItemComponent } from './header/recipes/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './header/recipes/recipe-detail/recipe-detail.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {ShoppingComponent} from './header/shopping/shopping.component';
+import {ShoppingListComponent} from './header/shopping/shopping-list/shopping-list.component';
+import {ShoppingListEditComponent} from './header/shopping/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import {RecipesComponent} from './header/recipes/recipes.component';
+import {RecipeItemComponent} from './header/recipes/recipe-item/recipe-item.component';
+import {RecipeDetailComponent} from './header/recipes/recipe-detail/recipe-detail.component';
 import {RecipeListComponent} from "./header/recipes/recipe-list/recipe-list.component";
 
 @NgModule({
@@ -26,7 +27,8 @@ import {RecipeListComponent} from "./header/recipes/recipe-list/recipe-list.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
