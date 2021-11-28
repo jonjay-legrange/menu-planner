f2c1b896892dc660f8dd540cdecce8d958c379ac
Jon Jay Le Grange <jonjay.legrange@blv.admin.ch>
2021-11-28 12:55:34 +0100

    Course Project - Routing Part 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { ShoppingComponent } from './components/shopping/shopping.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'shopping-list', component: ShoppingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
